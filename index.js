af61df02916b3665d7125357f3c823e44c55446f
Daryl Nauman <darylnauman@gmail.com>
2021-06-09 19:23:14 -0400

    used fs to create README file, wrote JSON data to file to test

--- index.js
+++ index.js
@@ -15,48 +15,50 @@ const questions = [
         message: 'What is the project description? ',
         name: 'description',
     },
-    {
-        type: 'input',
-        message: 'What are the installation instructions? ',
-        name: 'installInstructions',
-    },
-    {
-        type: 'input',
-        message: 'What is the usage information? ',
-        name: 'usageInfo',
-    },
-    {
-        type: 'input',
-        message: 'What are the contribution guidelines? ',
-        name: 'guidelines',
-    },
-    {
-        type: 'input',
-        message: 'What are the test instructions? ',
-        name: 'testInstructions',
-    },
+    // {
+    //     type: 'input',
+    //     message: 'What are the installation instructions? ',
+    //     name: 'installInstructions',
+    // },
+    // {
+    //     type: 'input',
+    //     message: 'What is the usage information? ',
+    //     name: 'usageInfo',
+    // },
+    // {
+    //     type: 'input',
+    //     message: 'What are the contribution guidelines? ',
+    //     name: 'guidelines',
+    // },
+    // {
+    //     type: 'input',
+    //     message: 'What are the test instructions? ',
+    //     name: 'testInstructions',
+    // },
     {
         type: 'list',
         message: 'What is the license? ',
         name: 'license',
         choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
     },
-    {
-        type: 'input',
-        message: 'What is your GitHub username? ',
-        name: 'userName',
-    },
-    {
-        type: 'input',
-        message: 'What is your email address? ',
-        name: 'email',
-    },
+    // {
+    //     type: 'input',
+    //     message: 'What is your GitHub username? ',
+    //     name: 'userName',
+    // },
+    // {
+    //     type: 'input',
+    //     message: 'What is your email address? ',
+    //     name: 'email',
+    // },
 
 ];
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, JSON.stringify(data), (err) => err ? console.log(err) : console.log('Success!'));
+}
 
 // TODO: Create a function to initialize app
 function init() {
@@ -65,7 +67,7 @@ function init() {
             questions
         )
         .then((answers) => {
-            console.log(JSON.stringify(answers));
+            writeToFile('README.md', answers);
         })
 };
 
