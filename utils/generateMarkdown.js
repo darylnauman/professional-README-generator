2f674a9f00be794b7ab8f87a5cd0e7d8b08636fc
Daryl Nauman <darylnauman@gmail.com>
2021-06-09 16:47:00 -0400

    Added questions, type of prompts and choices (if applicable), names to store inputs; output to console

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,3 +1,5 @@
+// https://choosealicense.com/licenses/
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
